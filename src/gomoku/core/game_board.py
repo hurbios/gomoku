979cbae51a7624110394bb46aa32c91b6306e362
hurbios <15719468+hurbios@users.noreply.github.com>
2025-11-02 22:53:59 +0200

    Fix linting errors

--- src/gomoku/core/game_board.py
+++ src/gomoku/core/game_board.py
@@ -13,10 +13,10 @@ class Board:
     def __count_pieces_direction(self, count:int, position:tuple[int, int], player:int, direction:tuple[int, int]):
         if count >= 4:
             return 4
-        if (self.__width < (position[0]+direction[0]) or
-            (position[0]+direction[0]) < 0 or
-            self.__height < (position[1]+direction[1]) or
-            (position[1]+direction[1]) < 0):
+        if any(iter([(self.__width < (position[0]+direction[0])),
+            ((position[0]+direction[0]) < 0),
+            self.__height < (position[1]+direction[1]),
+            ((position[1]+direction[1]) < 0)])):
             return count
         if self.__moves[position[0]+direction[0]][position[1]+direction[1]] is not player:
             return count
@@ -31,21 +31,35 @@ class Board:
         )
         return max_in_row >=4
 
+    # Returns tuple: (True if player piece was added, True if player wins)
     def add_move(self, move: tuple[int, int], player:int):
-        if ((self.__width < move[0]) or (move[0] < 0) or
-                (self.__width < move[1]) or (move[1] < 0) or
-                (self.__height < move[0]) or (move[0] < 0) or
-                (self.__height < move[1]) or (move[1] < 0)):
+        # Check that the move is within the game area boundaries
+        if any(iter([
+                (self.__width < move[0]),
+                (move[0] < 0),
+                (self.__width < move[1]),
+                (move[1] < 0),
+                (self.__height < move[0]),
+                (move[0] < 0),
+                (self.__height < move[1]),
+                (move[1] < 0)
+              ])):
             return False, False
+
+        # Check that no piece exists yet in the move coordinates
         if self.__moves[move[0]][move[1]]:
             return False, False
+        # Check that the player is valid
         if player not in [1,2]:
             return False, False
+
+        # Add move to players pieces and on game board
         self.__moves[move[0]][move[1]] = player
         if player == 1:
             self.__player1_pieces.append(move)
         else:
             self.__player2_pieces.append(move)
+
         return True, self.__player_wins(move, player)
 
     def get_player_pieces(self, player):
