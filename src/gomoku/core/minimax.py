b1ee60c2dbe7084b245a5b493ee712b352076618
hurbios <15719468+hurbios@users.noreply.github.com>
2025-11-17 23:03:46 +0200

    Refactor minimax

--- src/gomoku/core/minimax.py
+++ src/gomoku/core/minimax.py
@@ -1,42 +1,34 @@
 from gomoku.core.game_board import Board
 
-PLAYER = {
-    'USER': 1,
-    'AI': 2
-}
 MAX_DEPTH = 1
 MOVE_RANGE = 2
+
+def get_player(is_player1:int)->int:
+    return 1 if is_player1 else 2
 
 class Minimax:
     def __init__(self, board: Board):
         self.__board = board
 
-    def minimax(self, last_move, depth, player):
+    def minimax(self, last_move, depth, is_player1):
         last_move_score = self.__board.evaluate_state_after_move(last_move)
-        WIN_SCORES = {
-            "PLAYER1": [-7, -5],
-            "PLAYER2": [8, 5]
-        }
-        if depth <= 0 or (last_move_score in WIN_SCORES["PLAYER2"] and player == 1) or (last_move_score in WIN_SCORES["PLAYER1"] and player == 2):
+
+        if depth <= 0:
             return last_move_score, [last_move]
         moves = []
-        if player == 1:
-            low_score = 100
-            for coordinates in self.__board.get_surrounding_free_coordinates(last_move, MOVE_RANGE):
-                self.__board.add_move(coordinates,1)
-                move_score, moves_inner = self.minimax(coordinates, depth-1, 2)
-                self.__board.remove_move(coordinates,1)
+
+        low_score = 100
+        high_score = -100
+        for coordinates in self.__board.get_surrounding_free_coordinates(last_move, MOVE_RANGE):
+            self.__board.add_move(coordinates, get_player(is_player1))
+            move_score, moves_inner = self.minimax(coordinates, depth-1, not is_player1)
+            self.__board.remove_move(coordinates, get_player(is_player1))
+            if is_player1:
                 if move_score < low_score:
                     low_score = move_score
                     moves=moves_inner
                     moves.append(coordinates)
-
-        else:
-            high_score = -100
-            for coordinates in self.__board.get_surrounding_free_coordinates(last_move, MOVE_RANGE):
-                self.__board.add_move(coordinates,2)
-                move_score, moves_inner = self.minimax(coordinates, depth-1, 1)
-                self.__board.remove_move(coordinates,2)
+            else:
                 if move_score > high_score:
                     high_score = move_score
                     moves=moves_inner
@@ -49,8 +41,8 @@ class Minimax:
     def get_next_move(self, last_move):
         moves = []
         high_score = -100
-        # for coordinates in self.__board.get_surrounding_free_coordinates(last_move, MOVE_RANGE):
-        for coordinates in [(2,2),(3,2),(4,2)]:
+        # for coordinates in [(2,2),(3,2),(4,2)]:
+        for coordinates in self.__board.get_surrounding_free_coordinates(last_move, MOVE_RANGE):
             self.__board.add_move(coordinates,2)
             print(coordinates, self.__board.get_player_move_result(coordinates,2), self.__board.evaluate_state_after_move(coordinates))
             move_score, moves_inner = self.minimax(coordinates, MAX_DEPTH - 1, 1)
