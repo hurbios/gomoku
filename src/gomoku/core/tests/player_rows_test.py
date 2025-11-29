5c3f18360f487d7cf951ac5a7df4627e083fc135
hurbios <15719468+hurbios@users.noreply.github.com>
2025-11-29 21:26:03 +0200

    Add couple of unit tests

--- src/gomoku/core/tests/player_rows_test.py
+++ src/gomoku/core/tests/player_rows_test.py
@@ -18,6 +18,9 @@ class TestRow(unittest.TestCase):
 
     def test_moves(self):
         self.assertEqual(self.row.moves, [(2,0),(2,1),(2,2),(2,3)])
+    
+    def test_str(self):
+        self.assertEqual(str(self.row), "[(2, 0), (2, 1), (2, 2), (2, 3)]")
 
     # [0,0,0,0,0],
     # [0,2,0,0,0],
@@ -99,3 +102,42 @@ class TestRow(unittest.TestCase):
         self.assertEqual(board.player1_rows[0].score, 2000)
         self.assertEqual(board.player2_rows[0].score, 200)
         self.assertEqual(board.evaluate_state(1,(0,0),0), -1800)
+
+    # [1,0,0,0,0],
+    # [0,1,0,0,0],
+    # [0,0,1,0,0],
+    # [0,0,0,0,0]
+    # [0,0,0,0,0],
+    def test_row_direction(self):
+        row = Row([(1,1)], self.board)
+        row.add((0,0))
+        row.add((2,2))
+        self.assertEqual(row.get_direction((1,0)), 'horizontal')
+
+    # [3,3,0,0,0],
+    # [0,1,0,0,0],
+    # [0,0,1,0,0],
+    # [0,0,0,2,0]
+    # [0,0,0,0,2],
+    def test_row_join(self):
+        row = Row([(1,1)], self.board)
+        row.add((2,2))
+        row2 = Row([(4,4)], self.board)
+        row2.add((3,3))
+        row.join_row((3,3),row2)
+        row3 = Row([(1,0)], self.board)
+        row3.add((0,0))
+        row.join_row((0,0),row3)
+        self.assertEqual(row.moves, [(0,0),(1,1),(2,2),(3,3),(4,4)])
+    
+    # [0,0,0,0,0],
+    # [0,1,0,0,0],
+    # [0,0,1,0,0],
+    # [0,0,0,1,2]
+    # [0,0,0,0,0],
+    def test_row_join(self):
+        row = Row([(1,1)], self.board)
+        row.add((2,2))
+        row.add((3,3))
+        
+        self.assertEqual(row.get_touching_building_move((4,3), 'diagonal'), None)
