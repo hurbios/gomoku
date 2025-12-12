2e230fb7ec9ad3ed3c78b80959dccf82fcb877af
hurbios <15719468+hurbios@users.noreply.github.com>
2025-12-12 19:28:44 +0200

    Add game board tests for 100% coverage

--- src/gomoku/core/tests/game_board_test.py
+++ src/gomoku/core/tests/game_board_test.py
@@ -1,5 +1,5 @@
 import unittest
-# from unittest.mock import Mock, ANY
+from unittest.mock import Mock, MagicMock
 from gomoku.core.game_board import Board
 from gomoku.ui import game_board_ui
 
@@ -12,6 +12,9 @@ class TestGameBoard(unittest.TestCase):
         self.assertEqual(self.board.size(), (20,20))
         self.assertEqual(self.board.get_player_pieces(1), set())
         self.assertEqual(self.board.get_player_pieces(2), set())
+        self.assertEqual(self.board.height, 20)
+        self.assertEqual(self.board.width, 20)
+        self.assertEqual(self.board.moves, [[0 for _ in range(self.board.width)] for _ in range(self.board.height)])
 
     def test_add_move_horizontal_win(self):
         play = [
@@ -141,6 +144,10 @@ class TestGameBoard(unittest.TestCase):
         ok, has_won = self.board.add_move((game_board_ui.BLOCKS_PER_SIDE+1,1),1)
         self.assertEqual((ok, has_won), (False,False))
 
+    def test_add_move_incorrect_player(self):
+        ok, has_won = self.board.add_move((1,1),3)
+        self.assertEqual((ok, has_won), (False,False))
+
     def test_get_player_pieces(self):
         play = [
             [0,0,1,2,0],
@@ -187,6 +194,30 @@ class TestGameBoard(unittest.TestCase):
 
         self.assertEqual(len(self.board.player1_rows), 2)
         self.assertEqual(len(self.board.player2_rows), 6)
+    
+    def test_add_building_move_to_rows_touching_case(self):
+        self.board = Board(6,6)
+        
+        play = [
+            [0,0,1,2,0,0],
+            [0,0,1,2,0,0],
+            [0,1,1,2,0,0],
+            [0,1,0,2,0,0],
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0]
+        ]
+        for i,row in enumerate(play):
+            for y,col in enumerate(row):
+                self.board.add_move((y,i),col)
+        self.board._Board__get_players_surrounding_rows_in_directions = MagicMock(return_value={
+            'vertical':[self.board.player2_rows[0]],
+            'horizontal':[self.board.player2_rows[0]],
+            'inverse_diagonal':[],
+            'diagonal':[]
+            })
+        self.board.add_move((5,3),2)
+        self.assertEqual(len(self.board.player1_rows), 5)
+        self.assertEqual(len(self.board.player2_rows), 2)
 
     def test_remove_move(self):
         self.board = Board(6,6)
@@ -239,6 +270,45 @@ class TestGameBoard(unittest.TestCase):
         self.board.remove_move((2,2),2)
         self.assertEqual(len(self.board.player1_rows), 1)
         self.assertEqual(len(self.board.player2_rows), 1)
+    
+    def test_remove_move3(self):
+        self.board = Board(6,6)
+
+        play = [
+            [0,0,0,0,0,0],
+            [0,0,2,0,0,0],
+            [0,0,2,1,0,0],
+            [0,2,2,1,0,0],
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0]
+        ]
+        for i,row in enumerate(play):
+            for y,col in enumerate(row):
+                self.board.add_move((y,i),col)
+
+        self.assertEqual(len(self.board.player1_rows), 1)
+        self.assertEqual(len(self.board.player2_rows), 3)
+
+        self.board.remove_move((2,2),2)
+        self.assertEqual(len(self.board.player1_rows), 1)
+        self.assertEqual(len(self.board.player2_rows), 2)
+    
+    def test_get_surrounding_moves_of_moves_rows(self):
+        self.board = Board(6,6)
+
+        play = [
+            [0,0,0,0,0,0],
+            [0,0,2,0,0,0],
+            [0,0,2,1,0,0],
+            [0,2,2,1,0,0],
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0]
+        ]
+        for i,row in enumerate(play):
+            for y,col in enumerate(row):
+                self.board.add_move((y,i),col, True)
+
+        self.assertEqual(self.board.get_surrounding_moves_of_moves_rows((2,3),2), {(0, 3),(2, 4),(2, 0)})
 
     def test_add_move_real1(self):
         board = Board(7,7)
@@ -289,3 +359,95 @@ class TestGameBoard(unittest.TestCase):
         board.remove_move((0,3),2)
         self.assertEqual(board.get_player_pieces(1), {(0,1),(1,3)})
         self.assertEqual(board.get_player_pieces(2), {(0,0),(0,2),(0,4)})
+
+    def test_row_score(self):
+        board = Board(7,7)
+
+        play = [
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,1,1,1,0,0],
+            [0,0,2,0,0,0,0],
+            [0,0,0,2,0,0,0],
+            [0,0,0,0,0,0,0],
+        ]
+        for i,row in enumerate(play):
+            for y,col in enumerate(row):
+                board.add_move((y,i),col)
+
+        self.assertEqual(board.player1_rows[0].score, 50000000)
+        self.assertEqual(board.player2_rows[0].score, 200)
+        self.assertEqual(board.evaluate_state(), -49999800)
+
+        board.add_move((1,3),2)
+
+        self.assertEqual(board.player1_rows[0].score, 1000)
+        self.assertEqual(board.player2_rows[0].score, 50000000)
+        self.assertEqual(board.evaluate_state(), 49999000)
+
+        board.add_move((5,3),2)
+
+        self.assertEqual(board.player1_rows[0].score, 0)
+        self.assertEqual(board.player2_rows[0].score, 50000000)
+        self.assertEqual(board.evaluate_state(), 50000000)
+
+        board.remove_move((5,3),2)
+        self.assertEqual(board.player1_rows[0].score, 1000)
+        self.assertEqual(board.player2_rows[0].score, 50000000)
+        self.assertEqual(board.evaluate_state(), 49999000)
+
+        board.remove_move((1,3),2)
+        self.assertEqual(board.player1_rows[0].score, 50000000)
+        self.assertEqual(board.player2_rows[0].score, 200)
+        self.assertEqual(board.evaluate_state(), -49999800)
+
+    def test_add_move_inspect_area_refresh(self):
+        board = Board(7,7)
+
+        play = [
+            [2,0,0,0,0,0,0],
+            [1,0,0,0,0,0,0],
+            [2,0,0,0,0,0,0],
+            [0,1,0,0,0,0,0],
+            [2,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+        ]
+        for i,row in enumerate(play):
+            for y,col in enumerate(row):
+                board.add_move((y,i),col, True)
+
+        self.assertEqual(board.get_player_pieces(1), {(0,1),(1,3)})
+        self.assertEqual(board.get_player_pieces(2), {(0,0),(0,2),(0,4)})
+
+        board.add_move((0,3),2, True)
+        self.assertEqual(board.get_player_pieces(1), {(0,1),(1,3)})
+        self.assertEqual(board.get_player_pieces(2), {(0,0),(0,2),(0,3),(0,4)})
+        self.assertEqual(board.inspect_moves, {(1, 2),(2, 1),(1, 5),(1, 4),(2, 3),(0, 5),(2, 5),(2, 4),(3, 1),(1, 1),(2, 0),(0, 6),(3, 3),(2, 6),(2, 2),(1, 0),(1, 6),(3, 5)})
+
+    def test_is_move_part_of_winning_row(self):
+        self.board = Board(6,6)
+
+        play = [
+            [0,0,0,0,0,1],
+            [0,0,2,0,0,1],
+            [0,0,2,1,0,0],
+            [0,2,2,1,1,1],
+            [0,0,0,0,0,0],
+            [0,2,0,2,0,0]
+        ]
+        for i,row in enumerate(play):
+            for y,col in enumerate(row):
+                self.board.add_move((y,i),col, True)
+
+        self.assertEqual(self.board.is_move_part_of_winning_row((2,4),2), False)
+        self.assertEqual(self.board.is_move_part_of_winning_row((2,5),2), False)
+        
+        self.board.add_move((2,4),2, True)
+        self.assertEqual(self.board.is_move_part_of_winning_row((2,4),2), False)
+        self.assertEqual(self.board.is_move_part_of_winning_row((2,5),2), False)
+        
+        self.board.add_move((2,5),2, True)
+        self.assertEqual(self.board.is_move_part_of_winning_row((2,4),2), True)
+        self.assertEqual(self.board.is_move_part_of_winning_row((2,5),2), True)
