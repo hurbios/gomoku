77db50f4a2e1bd8f7b75ea7020a8a27bcd3487d8
hurbios <15719468+hurbios@users.noreply.github.com>
2026-01-29 20:57:36 +0200

    Improve tests to cover minimax score for checking if winning score is found with current iteration depth

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+ITER_DEPTH=6 poetry run pytest src/gomoku/core/tests/minimax_test.py::TestMinimax::test_get_next_move_should_attack_case2_with_iter_depth_6
 ITER_DEPTH=4 poetry run coverage run --branch -m pytest
 poetry run coverage report -m
